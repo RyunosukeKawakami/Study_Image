41986aa9b4a5b3615ca1880342f5b857be47e641
RyunosukeKawakami <k.r.doragon28@gmail.com>
2019-05-03 20:27:33 +0900

    create histogram

--- main.cpp
+++ main.cpp
@@ -12,38 +12,45 @@
 #endif
 using namespace cv;
 
-Mat Histogram(Mat*);
+void Histogram(const Mat&);
 
 int main(){
 
-//input image
+/***input image***/
     Mat image = imread("../lena.png");
     if(!image.data){
         printf("can not open file.");
         return -1;
     }
-    Mat mono;
-    cvtColor(image,mono,COLOR_BGR2GRAY);
 
-  //  Histogram(&image);
+    Histogram(image);
     imshow("Image",image);
-    imshow("GrayColor",mono);
     waitKey(0);
+    destroyAllWindows();
     return 0;
 }
 
-Mat Histogram(Mat* image){
-    int r[256],g[256],b[256];
-    for(int i = 0;  i < 256; i++){
-        r[i] = 0;
-        g[i] = 0;
-        b[i] = 0;
-    }
-    
-    for(int y = 0; y < image->rows ;y++){
-        for(int y = 0; y < image->rows ;y++){
+void Histogram(const Mat& image){
+    int hist[256];
+    int luminance = 0;
+    Mat mono = Mat::zeros(Size(image.cols,image.rows),CV_8UC1);
+    Mat graph = Mat::zeros(Size(300,300),CV_8UC1);
+    for(int i = 0; i < 256; i++)
+        hist[i] = 0;
+
+    for(int y = 0; y < image.rows ;y++){
+        for(int x = 0; x < image.cols ;x++){
+            luminance = (int)0.114*image.at<Vec3b>(y,x)[0] + 
+                             0.587*image.at<Vec3b>(y,x)[1] +
+                             0.299*image.at<Vec3b>(y,x)[2];
+            hist[luminance]++;
+            mono.at<Vec3b>(y,x) = luminance;
+            line(graph,Point(luminance,0),Point(luminance,hist[luminance]/50),Scalar(255),1,1);
+           // line(graph,Point(x+1,0),Point(255,255),Scalar(0),1,1);
             
         }
     }
 
+    imshow("graph",graph);
+    imshow("Mono",mono);
 }
