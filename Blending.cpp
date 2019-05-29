800dc309078185c623f31deeb995cdb4006535ad
RyunosukeKawakami <k.r.doragon28@gmail.com>
2019-05-29 20:20:53 +0900

    create trackbar

--- Blending.cpp
+++ Blending.cpp
@@ -1,12 +1,18 @@
 #include "Blending.h"
 
-Blending::Blending(const Mat& image):rows(image.rows),cols(image.cols){
+Blending::Blending(const Mat& image):rows(image.rows),cols(image.cols),trackbar_max(100){
     original[0] = image;
     original[1] = imread("../sample/Mandrill512.png");
+    track_num = 0;
+    alpha_num = 1;
 
     average = Mat::zeros(Size(rows,cols),CV_8UC3);
       alpha = Mat::zeros(Size(rows,cols),CV_8UC3);
      emboss = Mat::zeros(Size(rows,cols),CV_8UC3);
+    
+    namedWindow("alpha");
+    createTrackbar("Alpha","alpha",&track_num,trackbar_max);
+
 }
 
 void Blending::DrawAverage(){
@@ -20,22 +26,24 @@ void Blending::DrawAverage(){
     imshow("ave",average);
 }
 
-void Blending::DrawAlpha(float a){
-    if(!(0 <= a <= 1)){
+void Blending::DrawAlpha(){
+    if(!(0 <= alpha_num <= 1)){
         printf("range of alpha is not correct\n");
         return;
     }
+     alpha_num = (float)track_num / trackbar_max;
 
     for(int y = 0; y < cols; y++){
        for(int x = 0; x < rows;x++){
-           alpha.at<Vec3b>(y,x)[0] = a * original[0].at<Vec3b>(y,x)[0] + (1 - a) * original[1].at<Vec3b>(y,x)[0];
-           alpha.at<Vec3b>(y,x)[1] = a * original[0].at<Vec3b>(y,x)[1] + (1 - a) * original[1].at<Vec3b>(y,x)[1];
-           alpha.at<Vec3b>(y,x)[2] = a * original[0].at<Vec3b>(y,x)[2] + (1 - a) * original[1].at<Vec3b>(y,x)[2];
+           alpha.at<Vec3b>(y,x)[0] = alpha_num * original[0].at<Vec3b>(y,x)[0] + (1 - alpha_num) * original[1].at<Vec3b>(y,x)[0];
+           alpha.at<Vec3b>(y,x)[1] = alpha_num * original[0].at<Vec3b>(y,x)[1] + (1 - alpha_num) * original[1].at<Vec3b>(y,x)[1];
+           alpha.at<Vec3b>(y,x)[2] = alpha_num * original[0].at<Vec3b>(y,x)[2] + (1 - alpha_num) * original[1].at<Vec3b>(y,x)[2];
         }   
     }
     imshow("alpha",alpha);
 }
 
-void Blending::DrawEmboss(){
-
+void Blending::DrawEmboss(const Mat& image){
+    Mat negaposi = ~image;
+    imshow("negaposi",negaposi);
 }
